1c4930db00f6de29fe7e372f8879e6df5a3b941b
Bangkit Juang Raharjo <juangraharjo03@gmail.com>
2022-11-20 08:32:33 +0700

    add soft delete for mentor at course module

--- Modules/Course/Entities/MentorCourse.php
+++ Modules/Course/Entities/MentorCourse.php
@@ -4,10 +4,11 @@ namespace Modules\Course\Entities;
 
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class MentorCourse extends Model
 {
-    use HasFactory;
+    use HasFactory, SoftDeletes;
 
     protected $guarded = ['id'];
 
