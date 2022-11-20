bc1db1f34811b815c6f94fff77e16034e38fe83c
Bangkit Juang Raharjo <juangraharjo03@gmail.com>
2022-11-20 08:34:26 +0700

    add soft delete for requirement course

--- Modules/Course/Entities/RequirementCourse.php
+++ Modules/Course/Entities/RequirementCourse.php
@@ -4,10 +4,11 @@ namespace Modules\Course\Entities;
 
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class RequirementCourse extends Model
 {
-    use HasFactory;
+    use HasFactory, SoftDeletes;
 
     protected $guarded = ['id'];
 
