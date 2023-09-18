0ea4a0dcf41ad2a2470f47efef9b5be0032945b9
Bangkit Juang Raharjo <bangkitjuangraharjo@gmail.com>
2023-09-18 17:44:21 +0700

    new controller for new create category sertification

---
+++ Modules/Sertification/Entities/CategoryCertification.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace Modules\Sertification\Entities;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
+class CategoryCertification extends Model
+{
+    use HasFactory, SoftDeletes;
+
+    protected $fillable = [];
+    
+    protected static function newFactory()
+    {
+        return \Modules\Sertification\Database\factories\CategoryCertificationFactory::new();
+    }
+}
---
+++ Modules/Sertification/Http/Controllers/Api/CategorySertificationController.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace Modules\Sertification\Http\Controllers\Api;
+
+use Illuminate\Http\Request;
+use Illuminate\Http\Response;
+use Illuminate\Routing\Controller;
+use Modules\Sertification\Entities\CategorySertification;
+
+class CategorySertificationController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     * @return Response
+     */
+    public function index()
+    {
+        try {
+            
+        } catch (Exception $e) {
+            
+        }
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     * @param Request $request
+     * @return Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Show the specified resource.
+     * @param int $id
+     * @return Response
+     */
+    public function show($id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     * @param Request $request
+     * @param int $id
+     * @return Response
+     */
+    public function update(Request $request, $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     * @param int $id
+     * @return Response
+     */
+    public function destroy($id)
+    {
+        //
+    }
+}
