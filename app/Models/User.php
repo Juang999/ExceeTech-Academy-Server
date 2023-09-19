84e8a73a12d4c28a1a65b2ee11091a185559a530
Bangkit Juang Raharjo <bangkitjuangraharjo@gmail.com>
2023-09-19 22:20:28 +0700

    new relationship into images table

--- app/Models/User.php
+++ app/Models/User.php
@@ -56,4 +56,9 @@ class User extends Authenticatable implements MustVerifyEmail
     {
         return $this->hasMany(MentorCourse::class);
     }
+
+    public function Image()
+    {
+        return $this->hasOne(Image::class);
+    }
 }
