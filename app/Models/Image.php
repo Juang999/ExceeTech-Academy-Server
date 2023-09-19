bcf7342f8ac09bfe5057c2497128418121e8ec45
Bangkit Juang Raharjo <bangkitjuangraharjo@gmail.com>
2023-09-19 22:16:40 +0700

    new model and migration to save image

---
+++ app/Models/Image.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Image extends Model
+{
+    protected $fillable = ['photo'];
+
+    use HasFactory;
+
+    public function User()
+    {
+        return $this->belongsTo(User::class);
+    }
+}
